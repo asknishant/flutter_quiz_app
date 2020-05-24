3fc085ce3ca029e58a89666ac6af630050c9e72c
Nishant Kumar <asknishant.39@gmail.com>
2020-05-24 22:40:42 +0530

    Now user can start taking questions.

--- lib/result.dart
+++ lib/result.dart
@@ -1,32 +1,40 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-
   final int resultScore;
-  Result(this.resultScore);
+  final Function resetHandler;
+  Result(this.resultScore,this.resetHandler);
   
+
   String get resultPhrase {
-    String resultText = 'You did it'; 
+    String resultText = 'You did it';
 
-      if(resultScore <= 100) {
-        resultText = 'You are Awesome';
-      }  else if(resultScore <= 30) {
-        resultText = 'You are great';
-      } else if(resultScore <=50) {
-        resultText = 'Exellent';
-      }else{
-        resultText = 'overwhelming';
-      }
-
+    if (resultScore <= 100) {
+      resultText = 'overwhelming!!! Your Score is'+ resultScore.toString();
+    } else if (resultScore <= 30) {
+      resultText = 'You are great'+resultScore.toString();
+    } else if (resultScore <= 50) {
+      resultText = 'Exellent'+resultScore.toString();
+    } else {
+      resultText = 'You are Awesome'+resultScore.toString();
+    }
 
     return resultText;
   }
- 
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text(resultPhrase ,
-    style: TextStyle(fontSize: 36,fontWeight: FontWeight.bold),
-    textAlign: TextAlign.center)
+    return Center(
+        child: Column(children: <Widget>[
+          new Container(child: Card(color: Colors.blueGrey,),padding: EdgeInsets.all(20),),
+           new Container(child: Text(resultPhrase,
+            style: TextStyle(color: Colors.white,fontSize: 36,fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center),
+            ),
+          FlatButton(onPressed: resetHandler, child: Text('Restart Quiz!')),
+          FlatButton(onPressed: null, child: Text('Exit')),
+        ],
+      )
     );
   }
 }
